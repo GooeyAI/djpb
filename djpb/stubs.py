1f7252a484836330cf437fc9bdcaa3b6c123f21e
Dev Aggarwal <devxpy@gmail.com>
2020-09-22 17:09:47 +0530

    stubs

--- djpb/stubs.py
+++ djpb/stubs.py
@@ -3,11 +3,11 @@ import typing
 from django.db import models
 from google.protobuf.message import Message
 
-DjModel = models.Model
-DjModelType = typing.Type[models.Model]
+DjModel = typing.TypeVar("DjModel", bound=models.Model)
+DjModelType = typing.TypeVar("DjModelType", bound=typing.Type[models.Model])
 
-DjField = models.Field
-DjFieldType = typing.Type[models.Field]
+DjField = typing.TypeVar("DjField", bound=models.Field)
+DjFieldType = typing.TypeVar("DjFieldType", bound=typing.Type[models.Field])
 
-ProtoMsg = Message
-ProtoMsgType = typing.Type[Message]
+ProtoMsg = typing.TypeVar("ProtoMsg", bound=Message)
+ProtoMsgType = typing.TypeVar("ProtoMsgType", bound=typing.Type[Message])
