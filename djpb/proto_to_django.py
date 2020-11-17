cc858632af2628e890d0496d164d38ba3c4926b7
Dev Aggarwal <devxpy@gmail.com>
2020-11-17 22:56:00 +0530

    handle KeyError

--- djpb/proto_to_django.py
+++ djpb/proto_to_django.py
@@ -61,8 +61,13 @@ def _proto_to_django(proto_obj: ProtoMsg, django_obj: DjModel = None) -> SaveNod
             unset = False
 
         if unset:
-            if field_map[field_name].null:
-                setattr(django_obj, field_name, None)
+            try:
+                field = field_map[field_name]
+            except KeyError:
+                pass
+            else:
+                if field.null:
+                    setattr(django_obj, field_name, None)
             # leave "unset" fields as-is
             continue
 
